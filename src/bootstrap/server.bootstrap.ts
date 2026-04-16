b662737d20c86f162dd825446a634f760ebcf351
Felipe Guerrero <felipeguerrero1016@gmail.com>
2026-04-15 22:27:13 -0500

    cuarta actualizacion

--- src/bootstrap/server.bootstrap.ts
+++ src/bootstrap/server.bootstrap.ts
@@ -8,11 +8,20 @@ export class ServerBootstrap {
     this.app = app;
   }
 
-  initialize() {
-    const server = http.createServer(this.app);
-    const PORT = Number(process.env.PORT ?? 4000);
-    server.listen(PORT, () => {
-      console.log(`Server running at http://localhost:${PORT}`);
+  initialize(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      const server = http.createServer(this.app);
+      const PORT = Number(process.env.PORT ?? 4000);
+      server
+        .listen(PORT)
+        .on("listening", () => {
+          console.log(`Server running at http://localhost:${PORT}`);
+          resolve(true);
+        })
+        .on("error", (err) => {
+          console.log(`Se ha generado un error:${err}`);
+          reject(false);
+        });
     });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,46 @@ import { ServerBootstrap } from "./bootstrap/server.bootstrap";
 
 const serverBootstrap = new ServerBootstrap(app);
 serverBootstrap.initialize();
+
+/**
+ * Función tipo clasica
+ */
+async function startServer() {
+  try {
+    const instance = [serverBootstrap.initialize];
+    await Promise.all(instance);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/**
+ * Función tipo flecha
+ */
+
+const startServerFlecha = async () => {
+  try {
+    const instance = [serverBootstrap.initialize];
+    await Promise.all(instance);
+  } catch (error) {
+    console.log(error);
+  }
+};
+/**
+ * Invocación de funciones
+ */
+startServer();
+startServerFlecha();
+
+/**
+ * Función tipo autoinvocada
+ */
+
+(async () => {
+  try {
+    const instance = [serverBootstrap.initialize];
+    await Promise.all(instance);
+  } catch (error) {
+    console.log(error);
+  }
+})();
